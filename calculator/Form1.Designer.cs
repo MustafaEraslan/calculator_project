f25cdda13878af1ef97cf9df4d31fb518f4bac9c
= <=>
2021-01-06 12:48:39 +0300

    Calculator_Project

---
+++ calculator/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSum = new System.Windows.Forms.TextBox();
+            this.btnInc = new System.Windows.Forms.Button();
+            this.btnDec = new System.Windows.Forms.Button();
+            this.btnMul = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtSum
+            // 
+            this.txtSum.Location = new System.Drawing.Point(49, 29);
+            this.txtSum.Name = "txtSum";
+            this.txtSum.Size = new System.Drawing.Size(152, 20);
+            this.txtSum.TabIndex = 7;
+            this.txtSum.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtSum.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSum_KeyPress);
+            // 
+            // btnInc
+            // 
+            this.btnInc.Location = new System.Drawing.Point(227, 29);
+            this.btnInc.Margin = new System.Windows.Forms.Padding(2);
+            this.btnInc.Name = "btnInc";
+            this.btnInc.Size = new System.Drawing.Size(56, 19);
+            this.btnInc.TabIndex = 1;
+            this.btnInc.Text = "+";
+            this.btnInc.UseVisualStyleBackColor = true;
+            this.btnInc.Click += new System.EventHandler(this.btnInc_Click);
+            // 
+            // btnDec
+            // 
+            this.btnDec.Location = new System.Drawing.Point(227, 53);
+            this.btnDec.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDec.Name = "btnDec";
+            this.btnDec.Size = new System.Drawing.Size(56, 19);
+            this.btnDec.TabIndex = 2;
+            this.btnDec.Text = "-";
+            this.btnDec.UseVisualStyleBackColor = true;
+            this.btnDec.Click += new System.EventHandler(this.btnDec_Click);
+            // 
+            // btnMul
+            // 
+            this.btnMul.Location = new System.Drawing.Point(227, 76);
+            this.btnMul.Margin = new System.Windows.Forms.Padding(2);
+            this.btnMul.Name = "btnMul";
+            this.btnMul.Size = new System.Drawing.Size(56, 19);
+            this.btnMul.TabIndex = 3;
+            this.btnMul.Text = "X";
+            this.btnMul.UseVisualStyleBackColor = true;
+            this.btnMul.Click += new System.EventHandler(this.btnMul_Click);
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.Location = new System.Drawing.Point(227, 100);
+            this.btnDiv.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(56, 19);
+            this.btnDiv.TabIndex = 4;
+            this.btnDiv.Text = "/";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.btnDiv_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 158);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(149, 158);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 254);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.btnMul);
+            this.Controls.Add(this.btnDec);
+            this.Controls.Add(this.btnInc);
+            this.Controls.Add(this.txtSum);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "o";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSum;
+        private System.Windows.Forms.Button btnInc;
+        private System.Windows.Forms.Button btnDec;
+        private System.Windows.Forms.Button btnMul;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ calculator/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator
+{
+    public partial class Form1 : Form
+    {
+        int sum = 0;
+        int counterInc = 0;
+        int counterDec = 0;
+        int counterMul = 0;
+        bool opr = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+       
+         
+
+        private void btnInc_Click(object sender, EventArgs e)
+        {
+            counterInc++;
+            if (counterInc == 1)
+            {
+                sum = int.Parse(txtSum.Text);
+            }
+            else
+            {
+                sum = int.Parse(label1.Text) + int.Parse(txtSum.Text);
+                txtSum.Text = sum.ToString();
+            }
+            label1.Text = sum.ToString();
+
+            txtSum.Focus();
+            opr = true;
+        }
+       
+
+        private void btnDec_Click(object sender, EventArgs e)
+        {
+            counterDec++;
+            if (counterDec == 1)
+            {
+                sum = int.Parse(txtSum.Text);
+            }
+            else
+            {
+                sum = int.Parse(label1.Text) - int.Parse(txtSum.Text);
+                txtSum.Text = sum.ToString();
+
+            }
+            label1.Text = sum.ToString();
+            txtSum.Focus();
+            opr = true;
+            
+        }
+
+        
+        private void btnMul_Click(object sender, EventArgs e)
+        {
+            counterMul++;
+            if (counterMul == 1)
+            {
+                sum = int.Parse(txtSum.Text);
+            }
+            else
+            {
+                sum = int.Parse(label1.Text) * int.Parse(txtSum.Text);
+                txtSum.Text = sum.ToString();
+                txtSum.Focus();
+
+            }
+            label1.Text = sum.ToString();
+            opr = true;
+            
+        }
+        
+        private void txtSum_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (opr==true)
+            { 
+                txtSum.Text = "";
+                opr = false;
+            }
+            string gg = e.KeyChar.ToString();
+            //if (gg=="+")
+            //{
+            //    sumissue();
+            //}
+
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            counterDec++;
+            if (counterDec == 1)
+            {
+                sum = int.Parse(txtSum.Text);
+            }
+            else
+            {
+                sum = int.Parse(label1.Text) / int.Parse(txtSum.Text);
+                txtSum.Text = sum.ToString();
+
+            }
+            label1.Text = sum.ToString();
+            txtSum.Focus();
+            opr = true; 
+            
+
+        }
+    }
+}
